591e25c0955c27898af65518432a2304a194416f
kberg8 <kberg@Kelseys-iMac.local>
2014-05-11 13:26:01 -0700

    8

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,49 @@ $(document).ready(function() {
 	  window.location = $(this).find("option:selected").val();
 	});
 
-	new Maplace({
-	show_markers: true,
-	locations: [{
-		lat: 47.652427, 
-		lon: -117.347926,
-		zoom: 18
-		}]
-	}).Load();
-
-	console.log('Page Loaded. Lets Do this!');
-
-
-
 });
+
+function initialize() {
+
+	var myLatlng = new google.maps.LatLng(45.409066, -122.745926);
+	var mapOptions = {
+	    zoom: 15,
+	    center: myLatlng
+	}
+
+	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+	var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Professional Eye Care Associates of America (PECAA)</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Address</b><br /> address<br />address ' +
+      '</div>'+
+      '</div>';
+
+	  var infowindow = new google.maps.InfoWindow({
+	      content: contentString
+	  });
+
+	var marker = new google.maps.Marker({
+	    position: myLatlng,
+	    map: map,
+	    title: 'PECAA!'
+	});
+
+	google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  	});
+
+	}
+
+	function loadScript() {
+	  var script = document.createElement('script');
+	  script.type = 'text/javascript';
+	  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+	      'callback=initialize';
+	  document.body.appendChild(script);
+	}
+
+	window.onload = loadScript;
